45afe1639f0df94f3f7ce7f861cbfb2f0f4102c7
Dheerajram Chandra <dchandra1@hawk.iit.edu>
2019-06-13 01:44:51 -0400

    [CDR] First commit

---
+++ src/main/java/com/groupup/backend/groupupbackend/GroupupbackendApplication.java
@@ -0,0 +1,13 @@
+package com.groupup.backend.groupupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupupbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupupbackendApplication.class, args);
+	}
+
+}
